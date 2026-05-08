550b3a866be427db4bc354a7a64e6fa42de97e74
Robbert van Dalen <robbert@eigentime.nl>
2026-05-08 22:45:37 +0200

    double perf

--- src/main/java/org/jaust/main/Start.java
+++ src/main/java/org/jaust/main/Start.java
@@ -9,25 +9,27 @@ import java.util.function.DoubleBinaryOperator;
 import static java.lang.Math.*;
 
 public class Start {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         example(new DefaultContext(44100));
     }
     
     static void example(Context c) {
-        var s440 = c.genL(t -> t+1);
-        var s880 = c.genL(t -> t*2);
-        var add = c.binL(Long::sum);
+        var t1 = c.genD(t -> t+1);
+        var t2 = c.genD(t -> t*2);
+        var add = c.binD(Double::sum);
         
         var p =
-            s440.
-            par(s880).
+            t1.
+            par(t2).
             seq(add);
         
-        var s1 = p.apply()[0];
+        var s = p.apply()[0];
         
-        System.out.print(s1);
-        for (long i = 0; i < 5; i++) {
-            System.out.println(s1.longAt(i));
+        double t = 0.0;
+        
+        for (long i = 0; i < 10_000_000_000L; i++) {
+            t += s.longAt(i);
         }
+        System.out.println(t);
     }
 }
